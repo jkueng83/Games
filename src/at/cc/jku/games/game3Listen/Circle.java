838d2a4657a7de4b40dc7447066b4bf018f85332
jkueng83 <johannes.kueng@gmx.at>
2020-04-25 10:07:46 +0200

    Coach Listen (Teil 2)

---
+++ src/at/cc/jku/games/game3Listen/Circle.java
@@ -0,0 +1,35 @@
+package at.cc.jku.games.game3Listen;
+
+import org.newdawn.slick.Graphics;
+
+import java.util.Random;
+
+public class Circle {
+
+    private float x, y;
+    private float speed;
+    private int diameter;
+
+    public Circle() {
+        Random random = new Random();
+        this.x = random.nextInt(600);
+        this.y = 0;
+        this.speed = random.nextInt(40)+10;
+        this.diameter = random.nextInt(80) + 10;
+    }
+
+    public void render(Graphics graphics) {
+
+        graphics.drawOval(this.x, this.y, this.diameter, this.diameter);
+
+    }
+
+    public void update(int delta) {
+
+        this.y += (float) delta / 100.0 * this.speed;
+        if (this.y > 600) {
+            this.y = 0;
+        }
+
+    }
+}
---
+++ src/at/cc/jku/games/game3Listen/ObjetsGame3Listen.java
@@ -0,0 +1,83 @@
+package at.cc.jku.games.game3Listen;
+
+//import at.jku.games.firstgame.Objects;
+
+import at.cc.jku.games.game2.Rectangle;
+import org.newdawn.slick.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class ObjetsGame3Listen extends BasicGame {
+
+    private List<Rectangle> rectangles;
+    private List<Circle> circles;
+
+    public ObjetsGame3Listen(String title) {
+        super(title);
+    }
+
+    public static void main(String[] argv) {
+
+        try {
+            AppGameContainer container = new AppGameContainer(new ObjetsGame3Listen("Games 2"));
+            container.setDisplayMode(800, 800, false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+        Random random = new Random();
+
+        this.rectangles = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+
+            Rectangle rectangle = new Rectangle(random.nextInt(600), random.nextInt(600),
+                    random.nextInt(50));
+
+            this.rectangles.add(rectangle);
+
+        }
+
+        this.circles = new LinkedList<>();// ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            Circle circle = new Circle();
+            this.circles.add(circle);
+                    }
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+
+        for (Rectangle rectangle : this.rectangles) { // Rechtecke updaten
+            rectangle.update(delta);
+        }
+
+        for (Circle circle: this.circles) { // Kreise Updaten
+            circle.update(delta);
+        }
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+        for (Rectangle rectangle : this.rectangles) { // Rechtecke Zeichnen
+            rectangle.render(graphics);
+        }
+
+        for (Circle circle: this.circles) { // Kreise Zeichnen
+            circle.render(graphics);
+        }
+
+    }
+}

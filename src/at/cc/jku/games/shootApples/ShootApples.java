790ed9a0a12cb5ece5f986065201b87e9b741492
jkueng83 <johannes.kueng@gmx.at>
2020-05-04 18:01:18 +0200

    Version 1 Ohne Collision Shape

--- src/at/cc/jku/games/shootApples/ShootApples.java
+++ src/at/cc/jku/games/shootApples/ShootApples.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShootApples extends BasicGame {
+    // Version 1: ohne Collision Shape
 
     private List<Actor> actors;
     private List<FlyingDiamond> flyingDiamonds;
